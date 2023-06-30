9fc0758f6e720055561ebf3dd4ac09047c5f9679
John Barry <john.olivierb@gmail.com>
2023-06-29 20:02:20 -0400

    Fix red blue team not render in room info

--- src/app/components/RoomInfo.tsx
+++ src/app/components/RoomInfo.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-import supabase from '@/app/services/supabase';
-import { roomStore } from '@/app/stores/roomStore';
-import HeroGrid from './HeroGrid';
+import { useEffect, useState } from "react";
+import supabase from "@/app/services/supabase";
+import { roomStore } from "@/app/stores/roomStore";
+import HeroGrid from "./HeroGrid";
 
 interface Team {
   [key: string]: any;
@@ -12,35 +12,46 @@ interface RoomInfoProps {
 }
 
 const RoomInfo: React.FC<RoomInfoProps> = ({ roomid }) => {
-  const [roomData, setRoomData] = useState<{blue: Team, red: Team} | null>(null);
+  const [roomData, setRoomData] = useState<{ blue: Team; red: Team } | null>(
+    null
+  );
+  const [blue, setBlue] = useState<Team | {}>({});
+  const [red, setRed] = useState<Team | {}>({});
+
   const [error, setError] = useState<Error | null>(null);
   const { rooms } = roomStore();
-  const room = rooms[roomid]
+  const room = rooms[roomid];
 
   useEffect(() => {
     const getRoom = async () => {
       const { data: roomTead, error } = await supabase
-        .from('rooms')
-        .select('*')
-        .eq('id', roomid).single()
+        .from("rooms")
+        .select("*")
+        .eq("id", roomid)
+        .single();
 
-      //TODO FIX MISSING BLUE FOREIGN KEY
-        const { data: blue } = await supabase
-        .from('teams')
-        .select('*')
-        .eq('id', roomTead.blue).single()
-      
-        const { data: red } = await supabase
-        .from('teams')
-        .select('*')
-        .eq('id', roomTead.red).single()
+      const { data: blue } = await supabase
+        .from("teams")
+        .select("*")
+        .eq("id", roomTead.blue)
+        .single();
+
+      setBlue(blue);
+
+      const { data: red } = await supabase
+        .from("teams")
+        .select("*")
+        .eq("id", roomTead.red)
+        .single();
+
+      setRed(red);
 
       if (error) {
         setError(error);
       } else {
         setRoomData({
-          blue: roomTead.blue,
-          red: roomTead.red
+          blue: blue,
+          red: red,
         });
       }
     };
@@ -49,22 +60,27 @@ const RoomInfo: React.FC<RoomInfoProps> = ({ roomid }) => {
   }, [roomid]);
 
   useEffect(() => {
+    const colorToSetMethod: { [key: string]: (team: Team | {}) => void } = {
+      blue: setBlue,
+      red: setRed,
+    };
     const channel = supabase.channel(roomid);
+    channel
+      .on(
+        "postgres_changes",
+        {
+          event: "UPDATE",
+          schema: "public",
+          table: "teams",
+          filter: `room=eq.${roomid}`,
+        },
+        (payload) => {
+          const { new: team } = payload;
 
-    const subscription = channel.on('postgres_changes', {
-      event: 'UPDATE',
-      schema: 'public',
-      table: 'teams',
-      filter: `room=eq.${roomid}`
-    }, (payload) => {
-      const { new: team } = payload;
-  
-      if (team.color === 'blue') {
-        setRoomData(oldData => oldData ? ({ ...oldData, blue: team }) : { blue: team, red: {} });
-      } else {
-        setRoomData(oldData => oldData ? ({ ...oldData, red: team }) : { blue: {}, red: team });
-      }
-    }).subscribe();
+          (colorToSetMethod[team.color] || (() => {}))(team);
+        }
+      )
+      .subscribe();
 
     return () => {
       // Unsubscribe from room updates when component unmounts
@@ -92,11 +108,11 @@ const RoomInfo: React.FC<RoomInfoProps> = ({ roomid }) => {
       <div className="flex my-24 gap-12 justify-center">
         <div>
           <h2>Blue Team Selected Heroes:</h2>
-          <HeroGrid team={roomData.blue} color="blue" useTiles={true} />
+          <HeroGrid team={blue} color="blue" useTiles={true} />
         </div>
         <div>
           <h2>Red Team Selected Heroes:</h2>
-          <HeroGrid team={roomData.red} color="red" useTiles={true} />
+          <HeroGrid team={red} color="red" useTiles={true} />
         </div>
       </div>
     </>
