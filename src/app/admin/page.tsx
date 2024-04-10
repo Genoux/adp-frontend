2306a45043404b98a5858f25d93ae8114804b910
John Barry <john.olivierb@gmail.com>
2024-04-09 22:47:30 -0400

    Remove /admin path

--- src/app/admin/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useRouter } from 'next/navigation';
-
-export default function Admin() {
-  const router = useRouter();
-  router.push('/admin/rooms'); // Redirect on successful login
-}
