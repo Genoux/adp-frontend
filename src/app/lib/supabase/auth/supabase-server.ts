65330b2280ad0f39a5cd874aecfef492b8343dab
John Barry <john.olivierb@gmail.com>
2024-05-19 15:16:18 -0400

    Removed Admin Auth

--- src/app/lib/supabase/auth/supabase-server.ts
+++
@@ -1,9 +0,0 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-
-const serverComponentClient = () =>
-  createServerComponentClient({
-    cookies,
-  });
-
-export default serverComponentClient;
