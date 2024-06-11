fc21ebc5b9c322bfdb86fbd415c341b475e98458
John Barry <john.olivierb@gmail.com>
2024-06-11 12:55:26 -0400

    Use the checkImage on prod

--- src/app/api/checkImages/route.ts
+++ src/app/api/checkImages/route.ts
@@ -3,11 +3,13 @@ import { NextResponse } from 'next/server';
 import { champions } from '@/app/utils/champions';
 import axios from 'axios';
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+export const runtime = 'edge';
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? '';
 console.log('Base URL:', baseUrl);
 
 const getImageUrl = (imageName: string, type: 'splash' | 'tiles') => {
-  return `${baseUrl}/images/champions/${type}/${imageName
+  return `https://draft.tournoishaq.ca/images/champions/${type}/${imageName
     ?.toLowerCase()
     .replace(/\s+/g, '')
     .replace(/[\W_]+/g, '')}.webp`;
